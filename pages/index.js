bb9208029115921c40513a67a03963929c1b6357
John U <arjorb@gmail.com>
2023-01-10 12:28:58 +0200

    feat: creating wrapper layout

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
-import Head from "next/head";
+import Head from 'next/head';
+import Wrapper from '../components/layouts/Wrapper';
 export default function Home() {
   return (
     <>
       <Head>
         <title>Unit A</title>
         <meta
-          name="description"
+          name='description'
           content="Ape Unit was founded in 2010 in Berlin. Since 2017 Ape Unit's
                   Unit 8 has been working in the field of blockchain technology."
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1 className="text-center text-9xl underline font-semibold">Unit A</h1>
+      <Wrapper size='small'>Hello</Wrapper>
+      <Wrapper>Hello</Wrapper>
     </>
   );
 }

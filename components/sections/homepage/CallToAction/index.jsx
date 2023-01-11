7abd2c4390f1c249f208d7ca8e8b229084a68ee7
John U <arjorb@gmail.com>
2023-01-11 09:51:29 +0200

    remove the margin top on the paragraph and adding the font weight to the title

--- components/sections/homepage/CallToAction/index.jsx
+++ components/sections/homepage/CallToAction/index.jsx
@@ -6,13 +6,13 @@ const CallToAction = () => {
   return (
     <Wrapper size='small'>
       <div className='hidden lg:block w-full h-max bg-gray-8 rounded-4xl py-16 text-center mt-32'>
-        <h1 className='text-custom-3xl'>Ready to talk about your project?</h1>
-        <p className='text-center mx-auto text-custom-md mt-4.5 text-gray-1'>
+        <h1 className='text-custom-3xl font-semibold pb-0.25'>Ready to talk about your project?</h1>
+        <p className='text-center mx-auto text-custom-md text-gray-1'>
           Are you looking for a consultancy partnership to address your blockchain challenges? <br /> We’d love to hear
           from you. Let’s build something <br /> amazing together.
         </p>
         <div className='text-black w-full flex justify-center pt-16 px-5'>
-          <Button>Schedule a call</Button>
+          <Button size='large'>Schedule a call</Button>
         </div>
       </div>
     </Wrapper>

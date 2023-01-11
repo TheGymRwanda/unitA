feebe09f6477355182f60e00402a0c28a43f5549
Mutesi <ktesialine@gmail.com>
2023-01-11 15:24:51 +0200

    feat: about section and it's responsiveness

---
+++ components/sections/homepage/About/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Wrapper from "../../../layouts/Wrapper";
+import Button from "../../../ui/Button";
+import Title from "../../../ui/Title";
+
+const Index = () => {
+  return (
+    <Wrapper>
+      <div className="mt-23.75 md:mt-24.5 xl:mt-45.75">
+        <Title>Who we are</Title>
+        <div className="pt-1.5 pb-5 md:py-8.75 xl:py-4.5 text-2xl font-normal text-secondary md:text-custom-xl md:font-medium leading-25 -tracking-stretch">
+          <p className="md:pb-6">
+            Ape Unit was founded in 2010 in Berlin. Since 2017 Ape Unit's Unit 8
+            has been working in the field of blockchain technology. We are
+            working together with web3 protocols such as Ethereum, Tezos, Celo,
+            NEAR, Algorand, and Cosmos, but also for large international clients
+            such as VW and small African Startups like BitLipa.
+          </p>
+          <p>
+            With the new Unit A, we want to shift our focus to African
+            developers and create joint European and African organizations to
+            work in blockchain technology.
+          </p>
+        </div>
+        <Button link="mailto:u2467@apeunit.com">Contact us</Button>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Index;

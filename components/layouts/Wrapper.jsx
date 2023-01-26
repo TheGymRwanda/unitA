1234c20871de58630440394541c8ef5e4ab4a38d
Mutesi <ktesialine@gmail.com>
2023-01-26 11:54:21 +0200

    fix: remove unncessary percentage and screen

--- components/layouts/Wrapper.jsx
+++ components/layouts/Wrapper.jsx
@@ -3,7 +3,7 @@ import classNames from "classnames";
 const Wrapper = ({ children, size = "large" }) => {
   const wrapperStyle = classNames("xl:max-w-7xl mx-auto px-5 sm:px-11.5", {
     "xl:px-20 sm:px-11.5": size === "large",
-    "lg:px-11.5 1.1xl:px-0": size !== "large",
+    "lg:px-11.5 xl:px-0": size !== "large",
   });
 
   return <div className={wrapperStyle}>{children}</div>;

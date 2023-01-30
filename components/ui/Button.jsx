447386fd01b3db2e5cf8fe6a9d2ab3f495f7316e
John U <arjorb@gmail.com>
2023-01-30 15:28:44 +0200

    feat: changing the font size, font weight and letter spacing of hero section

--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -1,24 +1,21 @@
-import Image from "next/image";
-import Link from "next/link";
-import classNames from "classnames";
-const Button = ({ children, link = "#", target, size = "normal" }) => {
-  const buttonStyle = classNames(
-    "bg-white text-black -tracking-stretch flex w-fit px-8.75 lg:px-12 gap-6 group",
-    {
-      "py-4 text-md sm:text-3xl leading-11 rounded-5xl": size === "large",
-      "py-3 leading-9 text-2xl rounded-4xl": size !== "large",
-    }
-  );
+import Image from 'next/image';
+import Link from 'next/link';
+import classNames from 'classnames';
+const Button = ({ children, link = '#', target, size = 'normal' }) => {
+  const buttonStyle = classNames('bg-white text-black -tracking-stretch flex w-fit px-8.75 lg:px-12 gap-6 group', {
+    'py-4 text-md sm:text-3xl leading-11 rounded-5xl': size === 'large',
+    'py-3 leading-9 text-lg sm:text-2xl rounded-4xl': size !== 'large',
+  });
   return (
     <Link href={link} target={target}>
       <div className={buttonStyle}>
         {children}
         <Image
-          src={"/icons/arrow.svg"}
+          src={'/icons/arrow.svg'}
           width={28}
           height={28}
-          alt="arrow"
-          className="group-hover:rotate-45 transition-transform"
+          alt='arrow'
+          className='group-hover:rotate-45 transition-transform'
         />
       </div>
     </Link>

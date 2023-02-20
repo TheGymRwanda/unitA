85ad536a7df142906d9ea3b5c0a734f51c6bc60b
Jacob Dev <mercihabam@gmail.com>
2023-02-20 12:39:14 +0200

    fix: changed the button typography

--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 const Button = ({ children, link = '#', target, size = 'normal' }) => {
   const buttonStyle = classNames('bg-white text-black -tracking-stretch flex w-fit px-8.75 lg:px-12 gap-6 group', {
     'py-4 text-xl sm:text-3xl leading-7.5 sm:leading-9.5 rounded-5xl': size === 'large',
-    'py-3 leading-9 text-lg sm:text-2xl rounded-4xl': size !== 'large',
+    'py-3 text-lg sm:text-2xl rounded-4xl leading-6.75 lg:leading-7.5': size !== 'large',
   });
   return (
     <Link href={link} target={target}>
@@ -15,7 +15,7 @@ const Button = ({ children, link = '#', target, size = 'normal' }) => {
           width={28}
           height={28}
           alt='arrow'
-          className='group-hover:rotate-45 transition-transform leading-6.75 lg:leading-7.5 -tracking-stretch'
+          className='group-hover:rotate-45 transition-transform'
         />
       </div>
     </Link>
